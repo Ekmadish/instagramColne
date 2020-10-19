f54a22e3f2f91bd1565a0e4da88983678b728e33
Ekmadish <estaykargabay1206@gmail.com>
2020-10-19 11:47:00 +0600

    just Login pagee design

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -7,8 +7,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isSignedIn = false;
+
+  Widget buildHomeScreen() {
+    return Text("already signIn");
+  }
+
+  Widget buildSignScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColorDark
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "FlutterGram",
+              style: TextStyle(
+                  fontSize: 92, color: Colors.white, fontFamily: "Signatra"),
+            ),
+            GestureDetector(
+              onTap: () => "button was on tap",
+              child: Container(
+                width: 270,
+                height: 65,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/google_signin_button.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Home Page");
+    if (isSignedIn) {
+      return buildHomeScreen();
+    } else {
+      return buildSignScreen();
+    }
   }
 }
