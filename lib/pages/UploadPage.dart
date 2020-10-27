9683f1633466aeb49933146564181a0fe944c5e8
Ekmadish <estaykargabay1206@gmail.com>
2020-10-27 15:49:52 +0600

    JUSTcapture  image  page logic

--- lib/pages/UploadPage.dart
+++ lib/pages/UploadPage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
   @override
@@ -6,8 +9,97 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  File file;
+  displayUploadScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.add_photo_alternate,
+            color: Colors.green,
+            size: 200,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              onPressed: () => takeImage(context),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+              child: Text(
+                "Upload Image ",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              color: Colors.green,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  captureImage(ImageSource source) async {
+    final imagefile = await ImagePicker()
+        .getImage(source: source, maxHeight: 680, maxWidth: 970);
+
+    // Navigator.pop(context);
+    // File imagefile = await ImagePicker.pickImage(
+    //     source: source, maxHeight: 680, maxWidth: 970);
+    setState(() {
+      file = File(imagefile.path);
+    });
+
+    Navigator.pop(context);
+  }
+
+  takeImage(mcontext) {
+    return showDialog(
+      context: mcontext,
+      builder: (context) {
+        return SimpleDialog(
+          backgroundColor: Colors.white54,
+          title: Text(
+            "New Post",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          children: [
+            SimpleDialogOption(
+              onPressed: () => captureImage(ImageSource.camera),
+              child: Text(
+                "CaptureImage with Camera",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SimpleDialogOption(
+              onPressed: () => captureImage(ImageSource.gallery),
+              child: Text(
+                "Select Image  From Gallery",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                "Cancel",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Upload Page.");
+    return displayUploadScreen();
   }
 }
